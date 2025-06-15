bdf8ef1b9eb7c825a005fcbf5415fe843ccd67a1
Sarfaraz Alam <sarfaraz7277alam@gmail.com>
2025-06-15 11:46:27 +0530

    donloading api depend

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Main.css";
 import { assets } from "../../assets/assets";
 const Main = () => {
+  
   return (
     <div className="main">
       <div className="nav">
