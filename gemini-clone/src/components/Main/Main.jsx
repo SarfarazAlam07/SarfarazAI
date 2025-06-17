b0568841d51b7b1131bb5a6ef9cadc8cc6ca3a55
Sarfaraz Alam <sarfaraz7277alam@gmail.com>
2025-06-17 16:23:40 +0530

    name

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -17,7 +17,7 @@ const Main = () => {
   return (
     <div className="main">
       <div className="nav">
-        <p>Gemini</p>
+        <p>SarfarazAI</p>
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
